bd4f11f0462497352a81a7792f2fea1a8dc459c9
amitf_000 <amitf_000@Guptas>
2020-06-17 16:32:42 +0530

    Product Controller class

--- Product_Order_Fetch_MVC/src/Product/Product_Controller.java
+++ Product_Order_Fetch_MVC/src/Product/Product_Controller.java
@@ -3,14 +3,14 @@ import java.util.*;
 
 public class Product_Controller
 {
-	ArrayList<Product> arrList;
+	ArrayList<Product> arrList;//initializing variable arrlist of type Product	
+	ProductSearch_Output pso; //Initializing variable of type ProductSearch_Output
 	
-	ProductSearch_Output pso;
-	//constructor
+	//constructor 
 	public Product_Controller()
 	{
-		arrList=new ArrayList<Product>();
-		pso=new ProductSearch_Output();
+		arrList=new ArrayList<Product>(); //Object creation of ArrayList of type Product
+		pso=new ProductSearch_Output();   //Object Creation of ProductSearch_Output
 	}
 	//Getter and Setter Method
 	public ArrayList<Product> getArrList() {
@@ -18,14 +18,18 @@ public class Product_Controller
 	}
 	public void setArrList(ArrayList<Product> arrList) {
 		this.arrList = arrList;
-	}	
-	//Method for adding product
+	}
+	
+	//Method for adding product which takes Product object as argument 
+	// inside this creating object of ProductDAO class which has AddProduct method which adds the product to database
 	public void addProductController(Product pd)
 	{
 		ProductDAO pDAO=new ProductDAO();
 		pDAO.AddProduct(pd);
 	}
-	//Accept method for price input
+	//Accept method for input price range
+	//inside this Product object is initialized and then the processProductData method which takes price ranges as argument is called
+	//then displayData method is called
 	public void acceptData(double lPrice,double uPrice)
 	{
 		Product pd =new Product();
@@ -33,6 +37,7 @@ public class Product_Controller
 		displayData();
 		
 	}
+	//displayData method in this ProductSearch_Output() class display method is invoked which takes arrList object as argument
 	public void displayData()
 	{
 		pso.display(arrList);

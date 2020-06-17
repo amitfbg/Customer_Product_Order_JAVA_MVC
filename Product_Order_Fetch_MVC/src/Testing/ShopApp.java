e00fd9a885dfc2b8c2feb5753480c85f6135a039
amitf_000 <amitf_000@Guptas>
2020-06-17 17:51:29 +0530

    DataProcess_ShopApp class added for all the input data

--- Product_Order_Fetch_MVC/src/Testing/ShopApp.java
+++ Product_Order_Fetch_MVC/src/Testing/ShopApp.java
@@ -9,58 +9,9 @@ import Product.*;
 
 public class ShopApp {
 
-	public static void Add() {
-		try {
-			Product p = new Product();
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("Enter Product Code :");
-			String prodCode = br.readLine();
-			System.out.print("Enter Product Name :");
-			String prodName = br.readLine();
-			int prodWeight;
-			do {
-				System.out.print("Enter Product Weight :");
-				prodWeight = Integer.parseInt(br.readLine());
-				if (prodWeight < 0)
-					System.out.println("Wrong entry,Try Again");
-			} while (prodWeight < 0);
-			System.out.print("Enter Product Category :");
-			String prodCategory = br.readLine();
-			System.out.print("Enter MFG date (YYYY-MM-DD):");
-			Date mfgDate = Date.valueOf(br.readLine());
-			System.out.print("Enter Expire date (YYYY-MM-DD) :");
-			Date expDate = Date.valueOf(br.readLine());
-			double cost;
-			do {
-				System.out.print("Enter Product Cost :");
-				cost = Double.parseDouble(br.readLine());
-				if (cost <= 0)
-					System.out.println("Wrong entry,Try Again");
-			} while (cost <= 0);
-			p.setpName(prodName);
-			p.setpCode(prodCode);
-			p.setExpDate(expDate);
-			p.setMfgDate(mfgDate);
-			p.setpCatg(prodCategory);
-			p.setpWt(prodWeight);
-			p.setpCost(cost);
-			p.setQoh(0);
-			Product_Controller pc = new Product_Controller();
-			pc.addProductController(p);
-
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-	}
-	public static void Psearch()
-	{
-		ProductSearch_Input pInput = new ProductSearch_Input();
-		System.out.println("--*--Accepting Product Price Range--*--\n");
-		pInput.accept();
-	}
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
+		DataProcess_ShopApp dps=new DataProcess_ShopApp();
 		
 
 		int choice = 1;
@@ -71,10 +22,10 @@ public class ShopApp {
 			int select = sc.nextInt();
 			switch (select) {
 			case 1:
-				Add();
+				dps.Add();
 				break;
 			case 2:
-				Psearch();
+				dps.Psearch();
 				break;
 			}
 			System.out.println("Do you want to Continue:(1.Yes / 2.No ) :");

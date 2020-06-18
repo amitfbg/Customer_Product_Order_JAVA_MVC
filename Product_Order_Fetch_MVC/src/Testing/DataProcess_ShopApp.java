7c78abd1166280b3ed4c3ebacd1ea66f4ddb41fb
amitf_000 <amitf_000@Guptas>
2020-06-18 13:19:35 +0530

    DataProcrss customer value is taken from user

--- Product_Order_Fetch_MVC/src/Testing/DataProcess_ShopApp.java
+++ Product_Order_Fetch_MVC/src/Testing/DataProcess_ShopApp.java
@@ -4,16 +4,18 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.sql.Date;
 
+import Customer.Customer;
+import Customer.Customer_Controller;
 import Product.Product;
 import Product.ProductSearch_Input;
 import Product.Product_Controller;
 
 public class DataProcess_ShopApp {
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
 	public void Add() {
 		try {
 			Product p = new Product();
-			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 			System.out.print("Enter Product Code :");
 			String prodCode = br.readLine();
 			System.out.print("Enter Product Name :");
@@ -58,6 +60,49 @@ public class DataProcess_ShopApp {
 		ProductSearch_Input pInput = new ProductSearch_Input();
 		System.out.println("--*--Accepting Product Price Range--*--\n");
 		pInput.accept();
+	}
+	public void Customer_add()
+	{
+		try {
+		Customer cadd=new Customer();
+		System.out.println("--*--Accepting Customer Details--*--\n");
+		System.out.println("Enter Customer ID : ");
+		cadd.setCustID(br.readLine());
+		System.out.println("Enter Customer First Name : ");
+		cadd.setCustFName(br.readLine());
+		System.out.println("Enter Customer Last Name : ");
+		cadd.setCustLName(br.readLine());
+		String sex;
+		do {
+		System.out.println("Enter Sex of Customer (Male or Female) : ");
+		sex=br.readLine();
+		if(!sex.equalsIgnoreCase("Male") && !sex.equalsIgnoreCase("Female") )
+		{
+			System.out.println("Wrong Input !!!  Try Again with valid Input");
+		}
+		}while(!sex.equalsIgnoreCase("Male") && !sex.equalsIgnoreCase("Female"));
+		cadd.setSex(sex);
+		System.out.println("Enter the phone number of Customer : ");
+		cadd.setContactNo(Long.parseLong(br.readLine()));
+		System.out.println("Enter the City Details of Customer : ");
+		cadd.setCity(br.readLine());
+		String country;
+		do {
+		System.out.println("Enter the Country Details of Customer :(India) ");
+		country=br.readLine();
+		if(!country.equalsIgnoreCase("india"))
+		{
+			System.out.println("Wrong Entry!!! Enter Valid Details");
+		}
+		}while(!country.equalsIgnoreCase("india"));
+		cadd.setCountry(country);
+		Customer_Controller ccon=new Customer_Controller();
+		ccon.AddCustomer(cadd);
+		
+		}
+		catch(Exception e) {
+			System.out.println(e);
+		}
 	}
 
 }

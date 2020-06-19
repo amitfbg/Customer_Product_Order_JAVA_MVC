ad45ecf9664afcd71119ddc04f9c8b8e24034f3d
amitfbg <amitfbg570990@gmail.com>
2020-06-19 13:07:08 +0530

    void add method for product class removed

--- Product_Order_Fetch_MVC/src/Testing/DataProcess_ShopApp.java
+++ Product_Order_Fetch_MVC/src/Testing/DataProcess_ShopApp.java
@@ -13,48 +13,6 @@ import Product.Product_Controller;
 public class DataProcess_ShopApp {
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	
-	public void Add() {
-		try {
-			Product p = new Product();
-			System.out.print("Enter Product Code :");
-			String prodCode = br.readLine();
-			System.out.print("Enter Product Name :");
-			String prodName = br.readLine();
-			int prodWeight;
-			do {
-				System.out.print("Enter Product Weight :");
-				prodWeight = Integer.parseInt(br.readLine());
-				if (prodWeight < 0)
-					System.out.println("Wrong entry,Try Again");
-			} while (prodWeight < 0);
-			System.out.print("Enter Product Category :");
-			String prodCategory = br.readLine();
-			System.out.print("Enter MFG date (YYYY-MM-DD):");
-			Date mfgDate = Date.valueOf(br.readLine());
-			System.out.print("Enter Expire date (YYYY-MM-DD) :");
-			Date expDate = Date.valueOf(br.readLine());
-			double cost;
-			do {
-				System.out.print("Enter Product Cost :");
-				cost = Double.parseDouble(br.readLine());
-				if (cost <= 0)
-					System.out.println("Wrong entry,Try Again");
-			} while (cost <= 0);
-			p.setpName(prodName);
-			p.setpCode(prodCode);
-			p.setExpDate(expDate);
-			p.setMfgDate(mfgDate);
-			p.setpCatg(prodCategory);
-			p.setpWt(prodWeight);
-			p.setpCost(cost);
-			p.setQoh(0);
-			Product_Controller pc = new Product_Controller();
-			pc.addProductController(p);
-
-		} catch (Exception e) {
-			System.out.println(e);
-		}
-	}
 	public void Psearch()
 	{
 		ProductSearch_Input pInput = new ProductSearch_Input();

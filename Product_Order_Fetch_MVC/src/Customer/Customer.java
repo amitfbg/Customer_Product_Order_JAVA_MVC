8f8d55dd026555701fdc0a3524d4b282e81efccb
amitfbg <amitfbg570990@gmail.com>
2020-06-19 15:49:40 +0530

    Customer class updated

--- Product_Order_Fetch_MVC/src/Customer/Customer.java
+++ Product_Order_Fetch_MVC/src/Customer/Customer.java
@@ -1,147 +1,87 @@
 package Customer;
 
-import java.sql.*;
 public class Customer {
-	private String custID; 
+	private String custID;
 	private String custFName;
-	private String custLName; 
-	private String sex; 
-	private long contactNo; 
-	private String city; 
+	private String custLName;
+	private String sex;
+	private long contactNo;
+	private String city;
 	private String country;
 	private String message;
-	private static Connection con;
-	private static PreparedStatement pstate;
-	private static ResultSet rs;
+
 	public Customer() {
-		this.custID="";
-		this.custFName="";
-		this.custLName="";
-		this.sex="";
-		this.contactNo=0;
-		this.city="";
-		this.country="";
+		this.custID = "";
+		this.custFName = "";
+		this.custLName = "";
+		this.sex = "";
+		this.contactNo = 0;
+		this.city = "";
+		this.country = "";
 	}
+
 	public String getCustID() {
 		return custID;
 	}
+
 	public void setCustID(String custID) {
 		this.custID = custID;
 	}
+
 	public String getCustFName() {
 		return custFName;
 	}
+
 	public void setCustFName(String custFName) {
 		this.custFName = custFName;
 	}
+
 	public String getCustLName() {
 		return custLName;
 	}
+
 	public void setCustLName(String custLName) {
 		this.custLName = custLName;
 	}
+
 	public String getSex() {
 		return sex;
 	}
+
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
+
 	public long getContactNo() {
 		return contactNo;
 	}
+
 	public void setContactNo(long contactNo) {
 		this.contactNo = contactNo;
 	}
+
 	public String getCity() {
 		return city;
 	}
+
 	public void setCity(String city) {
 		this.city = city;
 	}
+
 	public String getCountry() {
 		return country;
 	}
+
 	public void setCountry(String country) {
 		this.country = country;
 	}
-	
-	static {
-		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			con=DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/office","root","password");
-			pstate=null;
-			rs=null;
-		}
-		catch(Exception e) {
-			System.out.println(e);
-		}
-	}
 
-	public static void display(String custID) {
-		try {
-			pstate=con.prepareStatement("Select * from Customer where custID=?");
-			pstate.setString(1, custID);
-			rs=pstate.executeQuery();
-			if(rs.next()) {
-				do {
-					System.out.println("-------------CUSTOMER DETAILS------------------------");
-					System.out.println("CustID:"+rs.getString(1));
-					System.out.println("Customer First Name:"+rs.getString(2));
-					System.out.println("Customer Last Name:"+rs.getString(3));
-					System.out.println("Sex:"+rs.getString(4));
-					System.out.println("Contact No:"+rs.getLong(5));
-					System.out.println("City:"+rs.getString(6));
-					System.out.println("Country:"+rs.getString(7));
-				}while(rs.next());
-			}
-			else {
-				System.out.println("Sorry!!!!No Record found");
-			}
-		}
-		catch(Exception e) {
-			System.out.println(e);
-		}
-	}
-	public static boolean search(String custID) {
-		
-		try {
-			pstate=con.prepareStatement("Select concat(custFName,' ',custLName) from Customer where custID=?");
-			pstate.setString(1, custID);
-			rs=pstate.executeQuery();
-			if(rs.next()) {
-				System.out.println("---CUSTOMER FOUND----");
-				System.out.println("Customer Name:"+rs.getString(1));
-				return  true;
-			}
-			else {
-				System.out.println("---CUSTOMER NOT FOUND----");
-				return false;
-			}
-		}
-		catch(Exception e) {
-			System.out.println(e);
-			return false;
-		}
-		
-	}
-	
-	public String toString(){
-		String s="";
-		s+=("Customer Id:"+this.custID+"\n");
-		s+=("Customer First Name:"+this.custFName+"\n");
-		s+=("Customer Last Name:"+this.custLName+"\n");
-		s+=("Sex:"+this.sex+"\n");
-		s+=("Contact No:"+this.contactNo+"\n");
-		s+=("City:"+this.city+"\n");
-		s+=("Country:"+this.country+"\n");
-		return s;
-	}
 	public String getMessage() {
 		return message;
 	}
+
 	public void setMessage(String message) {
 		this.message = message;
 	}
-	
 
 }
